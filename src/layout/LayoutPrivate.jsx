c47c2b0d689f7a4721f5ff5a463f1ffa90b070df
Jaaziel-Polanco <143868677+Jaaziel-Polanco@users.noreply.github.com>
2024-04-13 21:34:37 -0400

    Despliegue

---
+++ src/layout/LayoutPrivate.jsx
@@ -0,0 +1,8 @@
+import { Outlet } from "react-router-dom";
+
+const LayoutPrivate = () => {
+    return <>
+        <Outlet />
+    </>
+}
+export default LayoutPrivate;
---
+++ src/layout/LayoutRoot.jsx
@@ -0,0 +1,8 @@
+import { Outlet } from "react-router-dom";
+
+const LayoutRoot = () => {
+    return <>
+        <Outlet />
+    </>
+}
+export default LayoutRoot;
