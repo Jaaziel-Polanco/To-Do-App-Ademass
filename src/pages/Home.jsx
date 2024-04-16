f43d3a88639dfd8198508c89ccebaf91ed70a23c
Jaaziel-Polanco <143868677+Jaaziel-Polanco@users.noreply.github.com>
2024-04-16 11:26:25 -0400

    gh page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>Home en proceso...
+            <Link className='bg-primary rounded m-5' to="/login"> ir al Login</Link>
+        </div>
     )
 }
 
